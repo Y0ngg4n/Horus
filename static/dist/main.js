d33c49d3ee3c3daca1db0dae89e546f399576279
Yonggan <yonggan@obco.pro>
2023-02-20 22:42:40 +0100

    Add search of URL

--- static/dist/main.js
+++ static/dist/main.js
@@ -4,12 +4,14 @@ function SearchAndFilter() {
     var items = document.getElementsByClassName("service-item");
     var groups = document.getElementsByClassName("service-item-group");
     for (let x = 0; x < items.length; x++) {
+        let url = items[x].getElementsByClassName("service-item-url")[0]
         let item_name = items[x].getElementsByClassName("service-item-name")[0]
         let description = items[x].getElementsByClassName("service-item-description")[0]
         let group_item = items[x].parentElement.parentElement
         let group_item_label = group_item.getElementsByClassName("service-item-group-label")[0]
         if ((item_name && item_name.innerHTML.toUpperCase().indexOf(filter) > -1)
             || (description && description.innerHTML.toUpperCase().indexOf(filter) > -1)
+            || (url && url.innerHTML.toUpperCase().indexOf(filter) > -1)
             || (group_item_label && group_item_label.innerHTML.toUpperCase().indexOf(filter) > -1)) {
             items[x].style.display = "";
         } else {
