c79050e7b0fe9ea5140fc2d284cf906264b0bb54
Yonggan <yonggan@obco.pro>
2023-02-21 23:16:52 +0100

    Rewrite to classes

--- global_bookmarks.py
+++ global_bookmarks.py
@@ -1,34 +1,40 @@
 import kube
 
-def parse_global_bookmarks(config):
-    if "globalBookmarks" not in config:
-        return {}
-    bm = config["globalBookmarks"]
-    if bm:
-        group_bookmarks = {}
-        for group in bm:
-            name = group['group']
-            bookmarks = set([])
-            for bookmark in group["bookmarks"]:
-                url = bookmark["url"]
-                icon_url = kube.get_favicon(url)
-                if "icon" in bookmark:
-                    icon_url = bookmark["icon"]
-                target_blank = False
-                if "targetBlank" in bookmark:
-                    target_blank = bookmark["targetBlank"]
-                sub_pages = ""
-                if "subPages" in bookmark:
-                    sub_pages = bookmark["subPages"]
-                bookmarks.add(GlobalBookmark(name=bookmark["name"], url=url, icon_url=icon_url,
-                                             target_blank=target_blank, group=name, sub_pages=sub_pages))
-            group_bookmarks[name] = bookmarks
-        return group_bookmarks
-    else:
-        return {}
 
-def getSortedBookmarksList(list):
-    return sorted(list, key=lambda item: item.name)
+class GlobalBookMarks:
+    global_bookmarks: dict
+
+    def parse_global_bookmarks(self, config):
+        if "globalBookmarks" not in config:
+            return {}
+        bm = config["globalBookmarks"]
+        if bm:
+            group_bookmarks = {}
+            for group in bm:
+                name = group['group']
+                bookmarks = set([])
+                for bookmark in group["bookmarks"]:
+                    url = bookmark["url"]
+                    icon_url = kube.Kube.get_favicon(url)
+                    if "icon" in bookmark:
+                        icon_url = bookmark["icon"]
+                    target_blank = False
+                    if "targetBlank" in bookmark:
+                        target_blank = bookmark["targetBlank"]
+                    sub_pages = ""
+                    if "subPages" in bookmark:
+                        sub_pages = bookmark["subPages"]
+                    bookmarks.add(GlobalBookmark(name=bookmark["name"], url=url, icon_url=icon_url,
+                                                 target_blank=target_blank, group=name, sub_pages=sub_pages))
+                group_bookmarks[name] = bookmarks
+            self.global_bookmarks = group_bookmarks
+        else:
+            self.global_bookmarks = {}
+
+    @staticmethod
+    def get_sorted_bookmarks_list(list):
+        return sorted(list, key=lambda item: item.name)
+
 
 class GlobalBookmark:
     def __init__(self, name: str, group: str, url: str, icon_url: str, target_blank: bool, sub_pages: str):
