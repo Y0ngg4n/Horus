032b51164cd9cd8ab3a270dd9caa5662ac062b32
Yonggan <yonggan@obco.pro>
2023-02-25 22:54:08 +0100

    Debug ingress

--- kube.py
+++ kube.py
@@ -29,6 +29,7 @@ class Kube:
         v1 = client.NetworkingV1Api()
         if app_config["ingress"]["all"]:
             ret = v1.list_ingress_for_all_namespaces(watch=False)
+            print(ret)
             for i in ret.items:
                 parsed_ingress = self.parse_ingress(i)
                 if parsed_ingress:
